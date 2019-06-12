23011d807f96ef1373d5545d5a18963d87c41a4e
pinehat <pinehat@gmail.com>
2019-06-12 17:25:30 -0400

    work in progress

--- pybank/main.py
+++ pybank/main.py
@@ -6,7 +6,7 @@ import csv
 # Module for statistics calculations
 import statistics
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..', 'Resources', 'budget_data_test.csv')
   
 with open(csvpath, newline='') as csvfile:
 
@@ -18,26 +18,49 @@ with open(csvpath, newline='') as csvfile:
 
     net_total = 0
     row_count = 0
+
     diffs_list = []
+    month_list = []
     last_pl = 0
+
     
     # Read each row of data after the header
-    for row in csvreader:    
-        print(str(row_count))
+    for row in csvreader:
         net_total = net_total + int(row[1])
+    
         if row_count != 0:
+            month_list.append(row[0])
             diff_pl = int(row[1]) - last_pl
             diffs_list.append(diff_pl)
+
+            print(diff_pl)
+
+            print(f"Rowcount is {row_count}, PL is {int(row[1])}, Last PL is {last_pl}")
+
+            if int(row[1]) > last_pl:
+                max_row = row_count
+            if int(row[1]) < last_pl:
+                min_row = row_count
         
         last_pl = int(row[1])
         row_count = row_count + 1
 
-print(diffs_list)
-my_mean = statistics.mean(diffs_list)
-print("mean = " + str(my_mean))
-max_diff = max(diffs_list)
-print("max diff = " + str(max_diff))
+    print("Min row = " +str(min_row))
+    print("Max row = " +str(max_row))
 
+    #max_months = month_list(max_row)
+    #print(f" max months = {max_months}")
+
+    #for row in csvreader:
+    #    if row[1] == max_p_and_l:
+    
+    #       max_month = row[0]
+    #       print("found max month" + str(max_month))
+        #if diffs_list[1] == min_p_and_l:
+            #max_month = month_list[row]
+
+#print(diffs_list)
+print("")
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {row_count}")
---
+++ pypoll/main.py
@@ -0,0 +1,43 @@
+
+#PyPoll homework exercise main file
+
+import os
+
+# Module for reading CSV files
+import csv
+
+voter_id_list = []
+county_list = []
+candidate_list = []
+
+csvpath = os.path.join('..', 'Resources', 'election_data_test.csv')
+  
+with open(csvpath, newline='') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+
+    voter_id_list
+    # Read each row of data after the header
+    for row in csvreader:
+        voter_id_list.append(row[0])
+        county_list.append(row[1])
+        candidate_list.append(row[2])
+
+        row_counter = len(set(candidate_list))
+
+     #unique_candidates = set(candidate_list)
+       
+    # Print results to screen
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {len(voter_id_list)}")
+    print("-------------------------")
+    for row in set(candidate_list):
+        pass
+        #print(row)
+
+
+    
+
