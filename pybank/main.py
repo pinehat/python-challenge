184058fc7b4200986d26004ccf75600ffc54dde1
pinehat <pinehat@gmail.com>
2019-06-15 19:05:58 -0400

    Completed Python homework

--- pybank/main.py
+++ pybank/main.py
@@ -1,11 +1,13 @@
+# Module for finding/creating files
 import os
 
-# Module for reading CSV files
+# Module for reading delimited files
 import csv
 
 # Module for statistics calculations
 import statistics
 
+# Define input and output files
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 output_path = os.path.join("budget_analysis.txt")
   
@@ -17,65 +19,82 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
 
+    # define variable to increment total profits over period
     net_total = 0
+    # define variable to count number of records (months)
     row_count = 0
 
+    # list to keep track of the change in profits each month
     diffs_list = []
+    # list to store all records' months
     month_list = []
+
+    # Define variable to store each month's profit change as we process.
     last_pl = 0
 
-    # Read each row of data after the header
+    # Read each row of data after the header.
     for row in csvreader:
+        # increment row counter
         row_count = row_count + 1
-        net_total = net_total + int(row[1])
-        #month_list.append(row[0])
-        
+        # accrue profits/losses
+        net_total = net_total + int(row[1])      
+        # Starting with second row of data, add month to month list. Append to diffs_list
+        #   the difference between this month's and last month's profits.
         if row_count > 1:
             month_list.append(row[0])
-            diff_pl = int(row[1]) - last_pl
-            diffs_list.append(diff_pl)
+            diffs_list.append(int(row[1]) - last_pl)
 
-            #print(diff_pl)
+            #print(str(int(row[1]) - last_pl))
             #print(f"Rowcount is {row_count}, PL is {int(row[1])}, Last PL is {last_pl}")
         
+        # store current row's profit/loss for next iteration
         last_pl = int(row[1])
-    
-        
-    max_diff = max(diffs_list)
-    min_diff = min(diffs_list)
-
+ 
+    # Find the row number for which the change in profits was the greatest and the least.
     max_diff_row = diffs_list.index(max(diffs_list))
     min_diff_row = diffs_list.index(min(diffs_list))
 
+    # Using row numbers found above, find the corresponding months in the month_list for max & min.
     max_diff_month = month_list[max_diff_row]
     min_diff_month = month_list[min_diff_row]
 
+    # Reformat max and min months for screen and output file
+    month_split = max_diff_month.split("-")
+    max_diff_month = month_split[0] + "-20" + month_split[1]
+    month_split = min_diff_month.split("-")
+    min_diff_month = month_split[0] + "-20" + month_split[1]
     #print("Min row = " +str(min_diff_row))
     #print("Max row = " +str(max_diff_row))
 
+# Find and store the average monthly change in profits
 average_change = statistics.mean(diffs_list)
+
+# Find and store the max and min profit changes over the months
 greatest_increase = max(diffs_list)
 greatest_decrease = min(diffs_list)
 
+# Define function to format currency results as $0,000.00
 def as_currency(amount):
     if amount >= 0:
         return '${:,.2f}'.format(amount)
     else:
         return '-${:,.2f}'.format(-amount)
 
+# Print results to the screen
 print("")
 print("Financial Analysis")
 print("----------------------------")
-print(f"Data Over {row_count} Months")
+print(f"Profits and losses over {row_count} months")
 print(f"Total Profits: {as_currency(net_total)}")
 print(f"Average Monthly Change: {as_currency(average_change)}")
 print(f"Greatest Increase in Profits: {max_diff_month} {as_currency(greatest_increase)}")
 print(f"Greatest Decrease in Profits: {min_diff_month} {as_currency(greatest_decrease)}")
 
+# Write results to output file budget_analysis.txt
 with open(output_path, "w") as text:
     text.write("Financial Analysis" + "\n")
     text.write("---------------------------" + "\n")
-    text.write(f"Data over {row_count} months" + "\n")
+    text.write(f"Profits and losses over {row_count} months" + "\n")
     text.write(f"Total Profits: {as_currency(net_total)}" + "\n")
     text.write(f"Average Monthly Change: {as_currency(average_change)}" + "\n")
     text.write(f"Greatest Increase in Profits: {max_diff_month} {as_currency(greatest_increase)}" + "\n")
--- pypoll/main.py
+++ pypoll/main.py
@@ -1,26 +1,33 @@
-#PyPoll homework exercise main file
-
 # Module for input/output
 import os
 
 # Module for reading CSV files
 import csv
 
+# Define variable to count rows (votes)
 total_votes = 0
+
+# Define dictionary to store the unique candidates and number of their votes
 dict_cand_votes = {}
-dict_cand_percentage ={}
+# Define dictionary to store percentage of votes each candidate got
+dict_cand_percentage = {}
+
+# Define variable to compare candidates' votes
 winning_votes = 0
 
+# Define input and output paths and files
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 output_path = os.path.join("election_results.txt")
 
+# Open input file as delimited file
 with open(csvpath, newline='') as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
+    # create readable record
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    # get the header out of the way
     csv_header = next(csvreader)
 
-    # For each row, increment total votes and populate candidates dictionary
+    # For each row of data, increment total votes, get the candidate and populate candidate dictionary,
+    #  incrementing unique candidates' votes as we go
     for row in csvreader:
         total_votes = total_votes + 1
         candidate = row[2]
@@ -29,38 +36,37 @@ with open(csvpath, newline='') as csvfile:
         else:
             dict_cand_votes[candidate] = 1
 
-#print(f"dict_cand_votes is {dict_cand_votes}")
-#print(f"dict_cand_votes.items is {dict_cand_votes.items()}")
-
-# get candidates' percentage votes and the winner
-for cand, cand_votes in dict_cand_votes.items():
-
-    dict_cand_percentage[cand] = cand_votes / total_votes
-    if cand_votes > winning_votes:
-        winning_votes = cand_votes
-        winner = cand
-
-#print(f"dict_cand_percentage is {dict_cand_percentage}")
-
-# Display results
+# Display results to screen
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
+
+# For each listing in candidate votes dictionary, compute candidates' vote percentages and
+#  store in new dictionary, then compare candidates' votes to running total of winning votes
+#  to wind up with the the winner. Then print each candidate's votes and percentage, and
+#  finally the winner.
 for cand, cand_votes in dict_cand_votes.items():
+    dict_cand_percentage[cand] = cand_votes / total_votes
+    if cand_votes > winning_votes:
+        winning_votes = cand_votes
+        winner = cand
     print(f"{cand}: {dict_cand_percentage[cand]:.0%} ({cand_votes})")
+
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
-# Write election results to file
+# Write election results to output file election_results.txt
 with open(output_path, "w") as text:
     text.write("Election Results" + "\n")
     text.write("---------------------------" + "\n")
     text.write(f"Total Votes: {total_votes}" + "\n")
     text.write("---------------------------" + "\n")
+
     for cand, cand_votes in dict_cand_votes.items():
         text.write(f"{cand}: {dict_cand_percentage[cand]:.0%} ({cand_votes})" + "\n")
+
     text.write("---------------------------" + "\n")
     text.write(f"Winner: {winner}" + "\n")
     text.write("---------------------------" + "\n")
